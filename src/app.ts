9ed54162de4e7982022d02a545829b30ee868243
pantho0 <panthomashreky6@gmail.com>
2025-01-07 17:52:52 +0600

    server types error fixed and some bugs are fixed

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://quick-trip-two.vercel.app'],
     credentials: true,
   }),
 );
